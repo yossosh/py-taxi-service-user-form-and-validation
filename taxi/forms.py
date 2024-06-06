7a2cb0b03e02513f607c8ea4985b60a57533a4bb
Sofiia <sonagm9@gmail.com>
2024-06-06 04:53:22 +0300

    Fixed Flake8 Problem

--- taxi/forms.py
+++ taxi/forms.py
@@ -26,13 +26,16 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         """
         Validates the license number format.
 
-        The license number must consist of 3 uppercase letters followed by 5 digits.
+        The license number must consist of
+        3 uppercase letters followed by 5 digits.
 
         Raises:
-            forms.ValidationError: If the license number does not match the required format.
+            forms.ValidationError:
+            If the license number does not match the required format.
         """
         license_number = self.cleaned_data["license_number"]
         if not re.match(r"^[A-Z]{3}\d{5}$", license_number):
             raise forms.ValidationError(
-                "License number must consist of 3 uppercase letters followed by 5 digits."
+                "License number must consist of "
+                "3 uppercase letters followed by 5 digits."
             )
